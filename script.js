16f69c357f36c7506297e9b743480d10524a6ca6
mannen1234 <hugo.lundegard@elev.ga.ntig.se>
2024-01-30 13:01:19 +0100

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const button= document.querySelector("button");
+const body= document.querySelector("body");
+function changeColor(){
+    button.style.backgroundColor="Blue";
+    body.style.backgroundColor="Red";
+}
